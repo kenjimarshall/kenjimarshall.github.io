f3861aee9877053f7022fd87433cfb3e4b0fe3c8
Kenji Marshall <kenji.marshall99@gmail.com>
2020-04-21 00:41:26 -0400

    typing animation mark I

---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+function clickEffect(e) {
+    var d = document.createElement("div");
+    d.className = "clickEffect";
+    d.style.top = e.clientY - 10 + "px"; d.style.left = e.clientX - 10 + "px";
+    document.body.appendChild(d);
+    d.addEventListener('animationend', function () {
+        d.parentElement.removeChild(d);
+    }.bind(this));
+}
+
+document.addEventListener('click', clickEffect);
+
+
+
+$(document).ready(function () { //fade out
+    $("a").click(function (e) {
+        e.preventDefault();
+        $link = $(this).attr("href");
+        $(".container").fadeOut(200, function () {
+            window.location = $link;
+        });
+
+    });
+});
+
